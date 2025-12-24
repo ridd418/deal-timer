ef12d2cbe9b9bfa7fd4232f6e01c7df68588ef92
Ridd418 <147160638+ridd418@users.noreply.github.com>
2025-12-24 16:34:57 +0530

    B0: Finished JavaScript refactor and project overhaul

---
+++ modules/timer.js
@@ -0,0 +1,59 @@
+// Timer Factory Function.
+const createTimer = (displayUpdater, onEnd) => {
+
+    // State Variables
+    let clockInterval = null
+    let hours = 0, minutes = 0, seconds = 0
+
+    // Helper Functions
+    const clearTimer = () => {
+        clearInterval(clockInterval)
+        clockInterval = null
+    }
+
+    const setTimeVariables = (sec) => {
+
+        if (sec <= 0) {
+            hours = minutes = seconds = 0
+            return
+        }
+        
+        hours = Math.floor(sec / 3600)
+        const rem = sec % 3600
+        minutes = Math.floor(rem / 60)
+        seconds = rem % 60
+    }
+
+    // Methods
+    const start = (sec) => {
+        // Avoids simultaneous timer intervals or running when sec <= 0.
+        if (clockInterval || sec <= 0) return
+
+        setTimeVariables(sec)
+
+        const updateTicks = () => {
+            displayUpdater(hours, minutes, seconds)
+                
+            if (seconds > 0) { seconds-- }
+            else if (minutes > 0) { seconds = 59, minutes-- }
+            else if (hours > 0) { minutes = seconds = 59, hours-- }
+            else { clearTimer(), setTimeVariables(0), onEnd() }
+        }
+            
+        // Calling immediately for first run.
+        updateTicks()
+        clockInterval = setInterval(updateTicks, 1000)
+    }
+
+    const reset = () => {
+        clearTimer()
+        setTimeVariables(0)
+        displayUpdater()
+    }
+
+    const isRunning = () => clockInterval !== null
+
+    return { start, reset, isRunning }
+}
+
+export default createTimer
